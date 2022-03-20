b11202dfe290a1ea2a3ca7103a26ba3db1864033
junbeoman <wnsqja8744@gmail.com>
2022-03-21 01:59:16 +0900

    add calculation util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,8 @@ export const createQuery = (
   return `${queryName}=${queryValue}`;
 };
 
+export const meterToKilometer = (dist: number) => (dist / 1000).toFixed(1);
+
 export { default as debounce } from './debounce';
 export { default as userUtils } from './user';
 export { default as stopPropagation } from './stopPropagation';
