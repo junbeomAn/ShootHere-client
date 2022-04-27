f3acf907b9ade8211022bec65097d1dfd605e0c0
junbeoman <wnsqja8744@gmail.com>
2022-04-27 14:49:12 +0900

    modify IAddPlacePresenter interface to extend  isLoading property from IBase interface

--- src/AddPlace/AddPlace.entity.ts
+++ src/AddPlace/AddPlace.entity.ts
@@ -5,13 +5,13 @@ import {
   UseFormHandleSubmit,
   FieldValues,
 } from 'react-hook-form';
+import { IBase } from '../commonEntity';
 
-export interface IAddPlacePresenter {
+export interface IAddPlacePresenter extends IBase {
   register: UseFormRegister<FieldValues>;
   handleSubmit: (e?: React.BaseSyntheticEvent) => Promise<void>;
   onChange: (e: React.ChangeEvent) => void;
   errors: { [x: string]: any };
-  isLoading: boolean;
   isUploading: boolean;
   imageAsset: SanityAsset[];
 }
