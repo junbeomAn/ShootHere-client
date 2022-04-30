0d25eac74972ca3e2b66e855e7f8436203c08c9b
junbeoman <wnsqja8744@gmail.com>
2022-05-01 01:03:07 +0900

    add create query string util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,20 @@ export const createQuery = (
   return `${queryName}=${queryValue}`;
 };
 
+export function createQueryString<
+  T extends { [key: string]: string | number | undefined }
+>(params: T): string {
+  const qs = '?';
+
+  if (Object.keys(params).length === 0) return '';
+
+  return Object.keys(params).reduce((acc: string, key: string, i: number) => {
+    if (!params[key]) return acc;
+
+    return acc + `${i !== 0 ? '&' : ''}${key}=${params[key]}`;
+  }, qs);
+}
+
 export const meterToKilometer = (dist: number) => (dist / 1000).toFixed(1);
 
 export { default as debounce } from './debounce';
